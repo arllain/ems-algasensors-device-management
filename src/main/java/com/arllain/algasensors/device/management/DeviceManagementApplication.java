d76f39f66641b867adad12550aadb5d15ca0db33
Arllain <arllain@gmail.com>
2025-06-19 18:32:19 -0300

    New projects files

---
+++ src/main/java/com/arllain/algasensors/device/management/DeviceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.arllain.algasensors.device.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceManagementApplication.class, args);
+	}
+
+}
