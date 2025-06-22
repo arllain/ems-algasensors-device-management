bfc1bd40ad1f36ac3408b2292bb8fc19cd1728ae
Arllain <arllain@gmail.com>
2025-06-22 16:38:37 -0300

    Usando o TSID com Java

---
+++ src/main/java/com/arllain/algasensors/device/management/IdGenerator.java
@@ -0,0 +1,23 @@
+package com.arllain.algasensors.device.management;
+
+import io.hypersistence.tsid.TSID;
+
+import java.util.Optional;
+
+public class IdGenerator {
+
+    private static final TSID.Factory tsidfactory;
+
+    static  {
+        Optional.ofNullable(System.getenv("tsid.node")).ifPresent(tsidNode -> System.setProperty("tsid.node", tsidNode));
+        Optional.ofNullable(System.getenv("tsid.node.count")).ifPresent(tsidNodeCount -> System.setProperty("tsid.node.count", tsidNodeCount));
+
+        tsidfactory = TSID.Factory.builder().build();
+    }
+    private IdGenerator() {
+    }
+
+    public static TSID generateTSID() {
+        return tsidfactory.generate();
+    }
+}
---
+++ src/test/java/com/arllain/algasensors/device/management/TSIDTest.java
@@ -0,0 +1,20 @@
+package com.arllain.algasensors.device.management;
+
+import io.hypersistence.tsid.TSID;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+class TSIDTest {
+
+
+    @Test
+    public void shouldGenerateTSID() {
+
+        TSID tsid = IdGenerator.generateTSID();
+
+        Assertions.assertThat(tsid.getInstant()).isCloseTo(Instant.now(), Assertions.within(1, ChronoUnit.MINUTES));
+    }
+}
