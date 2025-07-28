da5546fa60514e91962be3188fdb6a4d746fa99b
Arllain <arllain@gmail.com>
2025-07-27 22:34:49 -0300

    Implementando a consulta do log de temperaturas

--- src/main/java/com/arllain/algasensors/device/management/domain/model/SensorId.java
+++ src/main/java/com/arllain/algasensors/device/management/domain/model/SensorId.java
@@ -3,14 +3,17 @@ package com.arllain.algasensors.device.management.domain.model;
 import io.hypersistence.tsid.TSID;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.util.Objects;
 
 @Getter
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
 public class SensorId implements Serializable {
 
     private TSID value;
